12f33b7b4ed9abd75c321cdb2de72e7cfed3d229
maga_bash <caitov.magomed@yandex.ru>
2023-02-02 22:35:26 +0300

    document append(img).

--- openweather.js
+++ openweather.js
@@ -1,3 +1,7 @@
+
 let div = document.createElement('div')
+let img = document.createElement('img')
+img.setAttribute('src', './svg/compass .svg')
 div.innerHTML = "<p>Привет погода</p>"
 document.body.append(div);
+document.body.append(img);
