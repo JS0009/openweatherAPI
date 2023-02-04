3420f6012f7d72396a095837cf842fd77956691d
maga_bash <caitov.magomed@yandex.ru>
2023-02-04 19:44:19 +0300

    mapFetch2

--- openweather.js
+++ openweather.js
@@ -1,9 +1,13 @@
 const weatherArray = ['https://api.openweathermap.org/data/2.5/weather?lat=55.7522&lon=37.6156&appid=1ccf606771220f99ee80e304554cf375', 'https://api.openweathermap.org/data/2.5/weather?lat=51.5085&lon=-0.12574&appid=1ccf606771220f99ee80e304554cf375']
 
 const url = weatherArray.map(async url => {
-    const respons = await fetch(url)
-    const data = await respons.json()
-    console.log(data)
+    try {
+        const respons = await fetch(url)
+        const data = await respons.json()
+        console.log(data)
+    } catch (err) {
+        console.log(err)
+    }
 })
 // let div = document.createElement('div')
 // let img = document.createElement('img')
