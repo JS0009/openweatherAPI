1f780fb319cb48d20f714eec1813d96cb7ab526e
maga_bash <caitov.magomed@yandex.ru>
2023-02-05 12:42:54 +0300

    Date

--- openweather.js
+++ openweather.js
@@ -4,15 +4,38 @@ const url = weatherArray.map(async url => {
     try {
         const respons = await fetch(url)
         const data = await respons.json()
-        console.log(data)
+
+
+        let div = document.createElement('div')
+        let h2 = document.createElement('h2')
+        let br = document.createElement('br')
+        let span = document.createElement('span')
+        let img = document.createElement('img')
+        let options = {
+            era: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            weekday: 'long',
+            timezone: 'UTC',
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+        }
+
+
+
+        const time = new Date(data.dt * 1000).toLocaleString('en-En', options)
+        img.setAttribute('src', './svg/compass .svg')
+        div.append(span.innerHTML = time)
+        div.append(br)
+        div.append(h2.innerHTML = data.name)
+
+        document.body.append(div, img);
+        document.body.style.backgroundColor = ''
+        document.body.style.display = 'block'
     } catch (err) {
         alert(err)
     }
-    let div = document.createElement('div')
-    let img = document.createElement('img')
-    img.setAttribute('src', './svg/compass .svg')
-    div.innerHTML = "<p>Привет погода</p>"
-    document.body.append(div, img);
-    document.body.style.backgroundColor = 'blue'
 })
 
